b85bffe34693e69b8b04e0e20b70c36d908dc6bb
Fabio <fabio.jfl16@gmail.com>
2021-05-28 21:15:13 -0300

    Created Person controller with crud.

---
+++ RestWithAspnet5/Controllers/PersonController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithAspnet5.Model;
+using RestWithAspnet5.Services;
+
+namespace RestWithAspnet5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<CalculatorController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if(person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithAspnet5/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,69 @@
+﻿using RestWithAspnet5.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestWithAspnet5.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            var persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                var person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Joao",
+                LastName = "Fabio",
+                Address = "Araucaria - PR",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Name" + i,
+                LastName = "Last Name" + i,
+                Address = "City - State - Country" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
